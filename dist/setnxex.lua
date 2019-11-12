fd498276e7a99ce628fdadb21f5e8c81f9489783
orange030 <530161671@qq.com>
2019-11-12 19:02:23 +0800

    auto release

--- dist/setnxex.lua
+++ dist/setnxex.lua
@@ -2,7 +2,7 @@ local key = KEYS[1]
 local expire = KEYS[2]
 local value = KEYS[3]
 
-if( redis.call('EXISTS',key) != 1 )then
+if( redis.call('EXISTS',key) ~= 1 )then
     redis.call('setex',key,expire,value) 
     return 1
 end
