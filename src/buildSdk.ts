c7d380cc9fbbb34f043a5da78ddfb407cb25fd3a
orange030 <orange030@gmail.com>
2021-02-05 04:18:11 +0800

    auto release

--- src/buildSdk.ts
+++ src/buildSdk.ts
@@ -49,7 +49,7 @@ function createCloudRunText(node:ts.MethodDeclaration,method = 'run',clientCache
         let clientCacheConfig = JSON.parse(clientCache) as {
             keyPath:string[][]
         }
-        let versionString = 'let version = '+(versionCb?`(${versionCb})(options!.clientCacheVersionParams).toString()`:'')
+        let versionString = 'let version = '+(versionCb?`(${versionCb})(options!.clientCacheVersionParams).toString()`:'""')
         let keyPath = `
         (${JSON.stringify(clientCacheConfig.keyPath)}.findIndex((e:string[]) => e.every(e => Object.keys(params||{}).includes(e)) && Object.keys(params||{}).every(e2 => e.includes(e2))) >= 0) ?
         "${functionName}"+(version&&("_"+version)||"")+"?"+`+"Object.keys(params||{}).map(e=>`${e}=${encodeURIComponent(params![e])}`).join('&')"+`
